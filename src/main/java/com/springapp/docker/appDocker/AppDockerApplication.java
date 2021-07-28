1509548218afedc607fc43d0706292092b13b46f
IagoAguiar <iagoaguiar202@gmail.com>
2021-07-28 19:25:46 -0300

    Iniciando a aplicacao

---
+++ src/main/java/com/springapp/docker/appDocker/AppDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.docker.appDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/docker/appDocker/AppDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.docker.appDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
